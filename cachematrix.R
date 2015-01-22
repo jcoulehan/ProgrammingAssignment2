0e6d84ab961998a050ef4abf5e3c8a376583dd74
jcoulehan <james.coulehan@intelematics.com>
2015-01-22 13:23:14 +1100

    Added comments to file

--- cachematrix.R
+++ cachematrix.R
@@ -1,8 +1,12 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This file contains two function used in compute the inverse of matrix 'x'.
+## makeCacheMatrix creates a list of supporting functions that support the setting/solving
+## cachSolve returns ths inverse of matrix 'X' . Its caches the input/output so as not recompute if input matrix is solved and not changed.
 
-## Write a short comment describing this function
-
+# Creates a list of functions to store and solve the inverse of matrix.
+# set - Sets the value of the input matrix
+# get      - Gets the value of the input matrix
+# setsolve - Sets the solved matrix for the given input.
+# getsolve - Gets the solved matrix that is the inverse of get
 makeCacheMatrix <- function(x = matrix()) {
   s <- NULL
   set <- function(y) {
@@ -18,12 +22,16 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+##This function return a matrix that is the inverse of 'x'
+# it first checks to see if there is already a solution computed and if the input matrix is identical to 
+# the matrix that was used to calculate the solved matrix in the cache.
+# The function assumes that the input matrix is inversable.
+cacheSolve <- function(x, mat) {
   s <- x$getsolve()
-  if(!is.null(s)) {
+  
+  #check to see if the inverse matrix has been solved. 
+  #It also check to see if the input matrix has changed or not
+  if((!is.null(s)) && identical(x$get(), mat)) {
     message("getting cached data")
     return(s)
   }
@@ -34,7 +42,14 @@ cacheSolve <- function(x, ...) {
 }
 
 
-
+# usage code showing the function in action.
 #mat <- rbind(c(1,4),c(1,1))
 #s <- makeCacheMatrix(mat)
-#cacheSolve(s)
+# calls solve the first time
+#cacheSolve(s, mat) 
+# mat as not changed so solve cache is used instead.
+#cacheSolve(s, mat)
+
+
+
+
